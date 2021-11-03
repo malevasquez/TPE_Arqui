92576d9f6d22549c08a804d95a541e356f8949c9
cami <csierra@itba.edu.ar>
2021-11-03 18:13:32 -0300

    videoDriver

--- Kernel/include/videoDriver.h
+++ Kernel/include/videoDriver.h
@@ -1,24 +1,18 @@
 #ifndef VIDEO_DRIVER_H
 #define VIDEO_DRIVER_H
 
-void startVideoDriver();
-
-void clearScreen();
-
 void drawPixel(int row, int col, int color);
 
-void drawRectangle(int x, int y, int width, int height, int col);
-
-void drawLine(int xStart, int yStart, int xEnd, int yEnd, int col);
-
-int drawMatrix(int * matrix, int row, int col, int rows, int columns);
-
-int getScreenHeight();
-
-int getScreenWidth();
+int drawCharFrom(int character, int row, int col, int color);
 
 int drawChar(int character, int color);
 
-int drawCharFrom(int character, int row, int col, int color);
+int drawMatrix(int *matrix, int row, int col, int rows, int columns);
+
+void clearScreen();
+
+int screenHeight();
+
+int screenWidth();
 
 #endif
