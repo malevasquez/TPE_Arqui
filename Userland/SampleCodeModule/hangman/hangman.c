a09c0ecb548fc423436cd86467c4c6acd112577b
magflores <magflores@itba.edu.ar>
2021-11-03 00:08:56 -0300

    Gotta catch 'em all!

--- Userland/SampleCodeModule/hangman/hangman.c
+++ Userland/SampleCodeModule/hangman/hangman.c
@@ -1,3 +1,50 @@
-#include <hangman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "hangman.h"
 
-static int ABC[26] = {0};
+//Cantidad de errores permitidos
+#define CHANCES 8
+
+//Longitud maxima que puede tener una palabra
+#define WORDLEN 20
+
+int main(void) {
+    char dictionary[][WORDLEN] = {"bulbasaur", "charmander", "cubone", "cyndaquil", "eevee", "gastly", "geodude", "growlithe", "jigglypuff", "meowth", "pidgey", "pikachu", "poliwhirl", "ponyta", "cefalopodo", "psyduck", "rattata", "snorlax", "squirtle", "wobbuffet"};
+    char letter, *word;
+    int mistakes = 0, qWords;
+    short found[WORDLEN] = {0};
+
+    qWords = sizeof(dictionary) / WORDLEN;
+    
+    int n;
+    
+    do {
+        n = getint("Enter a number from 1 to 20: ");
+    } while(n > 20 || n < 0);
+    
+    /* Elegir una palabra*/
+    word = dictionary[n - 1];
+
+    printf("\nGotta catch 'em all! Welcome to pokemon-hangman, the game where you have to guess the pokemon's name in order to win!\n");
+    printf("You have maximum 6 mistakes allowed :o Remember that all alphabet are in lower case\n");
+    printf("\nA wild pokemon has appeared! Good luck :)\n");
+
+    do {
+        printWord(word, found);
+        letter = readLetter();
+        if (!checkLetter(word, letter, found))
+        {
+            mistakes++;
+            if (CHANCES - mistakes > 0)
+                printf("Attack missed!\tRemaining attempts: %d\n", CHANCES - mistakes);
+        }
+        putchar('\n');
+    } while (mistakes < CHANCES && !completeWord(word, found));
+
+    if (mistakes == CHANCES)
+        printf("\n\nOh no! The wild %s fled! Better try next time :(\n", word);
+    else
+        printf("\n\nGotcha! %s was caught! Congrats!\n\n", word);
+
+    return 0;
+}
--- Userland/SampleCodeModule/include/hangman.h
+++ Userland/SampleCodeModule/include/hangman.h
@@ -1,9 +1,80 @@
-#ifndef HANGMAN_H
-#define HANGMAN_H
+#ifndef hangman_h
+#define hangman_h
 
-#include <stdGraphics.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <stdarg.h>
 
-#endif
+//Macro para vaciar el buffer
+#define BORRA_BUFFER while (getchar() != '\n')
+
+int getint(const char message[], ... ) {
+    int n, leave = 0;
+    va_list ap;
+        
+    do {
+        va_start(ap, message);
+        vprintf(message, ap);
+        va_end(ap);
+
+        if ( scanf("%d",&n) != 1)
+        {
+            printf("\nDato incorrecto\n");
+            BORRA_BUFFER;
+        }
+        else
+            leave = 1;
+    } while (! leave);
+    BORRA_BUFFER;
+    return n;
+}
+
+char readLetter(void)
+{
+    char letter;
+    printf("Please enter a letter: ");
+    letter = getchar();
+    if(letter != '\n')
+        BORRA_BUFFER;
+    return letter;
+}
+
+int checkLetter(const char word[], char letter, short found[])
+{
+    int i, ok = 0;
+
+    for (i = 0; word[i]; i++)
+        if (word[i] == letter)
+        {
+            ok = 1;
+            found[i] = 1;
+        }
+    return ok;
+}
+
+void printWord(const char word[], const short found[])
+{
+    int i;
+
+    for (i = 0; word[i]; i++)
+    {
+        if (found[i])
+            putchar(word[i]);
+        else
+            putchar('_');
+        putchar(' ');
+    }
+    putchar('\n');
+}
+int completeWord(const char word[], const short found[])
+{
+    int i, complete = 1;
+
+    for (i = 0; word[i] && complete; i++)
+        if (found[i] == 0)
+            complete = 0;
+
+    return complete;
+}
+
+#endif 
